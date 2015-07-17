8493e78c78cc1568b486ccf9cc3084cb2df62f90
Hans-Christoph Steiner <hans@eds.org>
2015-07-16 21:45:42 -0700

    update ./setup-ant.sh to set target and projectname from source files
    This makes it so there is one canonical place to set SDK target and the
    APK name.

--- setup-ant.sh
+++ setup-ant.sh
@@ -1,9 +1,33 @@
 #!/bin/sh
 
-for f in `find external/ -name project.properties`; do
-    android update lib-project -p `dirname $f` -t android-19
+set -e
+
+if ! which android > /dev/null; then
+    if [ -z $ANDROID_HOME ]; then
+        if [ -e ~/.android/bashrc ]; then
+            . ~/.android/bashrc
+        else
+            echo "'android' not found, ANDROID_HOME must be set!"
+            exit
+        fi
+    else
+        export PATH="${ANDROID_HOME}/tools:$PATH"
+    fi
+fi
+
+projectname=`sed -n 's,.*name="app_name">\(.*\)<.*,\1,p' app/res/values/strings.xml`
+# fetch target from project.properties
+eval `grep '^target=' app/project.properties`
+
+echo "Setting up build for $projectname"
+echo ""
+
+for f in `find * -name project.properties`; do
+    projectdir=`dirname $f`
+    echo "Updating ant setup in $projectdir:"
+    android update lib-project -p $projectdir -t $target
 done
-android update project -p app/ -t android-19 -n InformaCam --subprojects
+android update project -p . --subprojects --name "$projectname" --target $target
 
 cp external/InformaCam/libs/android-support-v4.jar external/ActionBarSherlock/actionbarsherlock/libs/android-support-v4.jar
 cp external/InformaCam/libs/android-support-v4.jar external/InformaCam/external/OnionKit/libnetcipher/libs/android-support-v4.jar
